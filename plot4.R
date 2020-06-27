8ee0a62c1276c612db15590f1e91a19fe6eace34
Sakib Malik <50263227+sakib-malik@users.noreply.github.com>
2020-06-27 17:25:26 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,8 +1,7 @@
 #code for loading Data
-
 library(chron)
 col <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-df <- read.table(file = "./household_power_consumption.txt",sep = ";", skip = 1, col.names = col)
+df <- read.table(file = "./household_power_consumption.txt",sep = ";", skip = 1, col.names = col,na.strings = "?", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df$Time <- chron(times = df$Time)
 df <- df[(grepl("^2007-02-0[12]", df$Date)),]
